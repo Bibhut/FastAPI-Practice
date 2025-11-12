e6372192071b5a5a5cc6fd2da10c9ffc97fba2ad
Bibhut <erbthakuri@gmail.com>
2025-11-12 08:57:32 +0545

    feat: booleans and operators practice done

---
+++ booleans_and_operators.py
@@ -0,0 +1,18 @@
+"""
+Boolean and Operators
+"""
+
+
+#Comparision operators
+print(1 == 2) # equal operator
+print(1 != 2) # not equal operator
+print(1 > 2) # greater than operator
+print(1 < 2) # less than operator
+print(1 >= 2) # greater than or equal to operator
+print(1 <= 2) # less than or equal to operator
+
+#Logical Operators
+
+print(1 > 3 and 5 <7) # in and operator both condition must be true to get true value
+print(1 > 3 or 5 <7) # in or operator anyb one  condition must be true to get true value
+print(not(1 == 1)) # not operator.
